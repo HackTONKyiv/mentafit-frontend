37a33420f4d995c3307edb1330cb75286d17cd96
Kastet <ostap99.2002@gmail.com>
2024-11-10 05:51:47 +0200

    ModalPopup unfinished

--- src/components/ModalPopup.tsx
+++ src/components/ModalPopup.tsx
@@ -1,4 +1,6 @@
 import "./ModalPopup.css";
+import TickDark from '../assets/TickDark.png';
+import CrossGreen from '../assets/CrossGreen.png';
 
 
 interface ModalPopupProps {
@@ -11,15 +13,16 @@ interface ModalPopupProps {
 
 export function ModalPopup({ onClose = () => {}, text = "Some text", onOk = () => {}, onCancel = () => {} } : ModalPopupProps) {
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={onClose}>&times;</span>
-        <p>{text}</p>
-        <div className="buttons">
-          <button onClick={onOk}>Ok</button>
-          <button onClick={onCancel}>Cancel</button>
+      <div className="modal">
+        <div id="overlay" className="overlay"></div>
+        <div className="modal-content">
+          {/*<span className="close" onClick={onClose}>&times;</span>*/}
+          <p>{text}</p>
+          <div className="buttons">
+            <button onClick={onOk}><img src={TickDark} alt={"Confirm button"}/></button>
+            <button onClick={onCancel}><img src={CrossGreen} alt={"Cancel button"}/></button>
+          </div>
         </div>
       </div>
-    </div>
   )
 }
