16911b0566daf7671da87e20a2aedf9203a31aa6
Kastet <ostap99.2002@gmail.com>
2024-11-10 03:59:47 +0200

    Added AddHabit styles

--- src/components/AddHabit.tsx
+++ src/components/AddHabit.tsx
@@ -15,18 +15,18 @@ export function AddHabit({ onClose = () => {}, onSubmit = () => {} } : AddHabitP
         <>
             <div className={"create-habit-popup"}>
                 <button className={"create-habit-button"} onClick={onClose}>&times;</button>
-                <input type="text" className={"habit-input"} placeholder={"Your new habit name"} value={habitName} onChange={(e) => setHabitName(e.target.value)}/>
+                <input type="text" className={"habit-input"} placeholder={"Write a new habit"} value={habitName} onChange={(e) => setHabitName(e.target.value)}/>
                 <p>repeat every</p>
                 <div className={"repeat-row"}>
                     <input type={"number"} className={"input-number"} value={habitInterval} min="1" onChange={(e) => setHabitInterval(parseInt(e.target.value))}/>
-                    <select name="interval" id="interval" value={habitIntervalType} onChange={(e) => setHabitIntervalType(e.target.value)}>
+                    <select name="interval" id="interval" className={"interval"} value={habitIntervalType} onChange={(e) => setHabitIntervalType(e.target.value)}>
                         <option value="hour">hour</option>
                         <option value="day">day</option>
                         <option value="week">week</option>
                         <option value="month">month</option>
                     </select>
                 </div>
-                <button className={"create-habbit-button"} onClick={() => onSubmit(habitName, habitInterval, habitIntervalType)}>Create</button>
+                <button className={"add-habit-button"} onClick={() => onSubmit(habitName, habitInterval, habitIntervalType)}>Create</button>
             </div>
         </>
 
